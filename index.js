d8d7af16d9cde0eb748b476987b3b6cedc7d2882
Jain4556 <sunil925business@gmail.com>
2026-03-07 20:37:43 +0530

    updated index and added packages

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-console.log("suniljain.me");
+import dotenv from "dotenv";
+import express from "express"
 
 
+dotenv.config()
+
+
+const app = express()
+const PORT  = process.env.PORT
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT} in ${process.env.NODE_ENV} mode`);
+    
+})
