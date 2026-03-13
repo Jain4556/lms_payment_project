f6029126ef9b56ed082eab0517263ef9f233ebb9
Jain4556 <sunil925business@gmail.com>
2026-03-13 17:00:26 +0530

    added course Progess Schema

--- models/courseProgress.js
+++ models/courseProgress.js
@@ -4,17 +4,17 @@ const lectureProgressSchema = new mongoose.Schema({
     lecture: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Lecture',
-        required :[true, 'Lecture reference is required']
-    }, 
+        required: [true, 'Lecture reference is required']
+    },
 
     isCompleted: {
-       type:  Boolean, 
-       default: false
-    } , 
+        type: Boolean,
+        default: false
+    },
     watchTime: {
         type: Number,
         default: 0
-    }, 
+    },
     lastWatched: {
         type: Date,
         default: Date.now()
@@ -23,5 +23,55 @@ const lectureProgressSchema = new mongoose.Schema({
 
 
 const courseProgessSchema = new mongoose.Schema({
-   
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: [true, "user reference is required"]
+    },
+    course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "course",
+        required: [true, "course reference is required"]
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
+    completedPercentage: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 100
+    },
+    lectureProgess: [lectureProgressSchema],
+    lastAccessed: {
+        type: Date,
+        default: Date.now()
+    }
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 })
+
+// calculate course completion
+courseProgessSchema.pre('save', function (next) {
+    if (this.lectureProgess.length > 0) {
+        const completedLectures = this.lectureProgess.filter(lp => lp.isCompleted).length
+        this.completionPercentage = Math.round((completedLectures / this.lectureProgess.length) * 100)
+        this.isCompleted = this.completionPercentage === 100
+
+    }
+    next()
+})
+
+
+// update last accessed
+courseProgessSchema.methods.updateLastAccessed = function () {
+    this.lastAccessed = Date.now()
+    return this.save({ ValidateBeforeSave: false })
+}
+
+
+export const courseProgess = mongoose.model
+
