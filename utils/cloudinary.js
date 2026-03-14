448e9be1c60e7a298fd1df381d53765e89287ba6
Jain4556 <sunil925business@gmail.com>
2026-03-14 16:18:21 +0530

    added cloudinary connection

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -31,17 +31,16 @@ export const uploadMedia = async function (file) {
 
 export const deleteMediaFromCloudinary = async (publicId) => {
     try {
-       await cloudinary.uploader.destroy(publicId) 
+        await cloudinary.uploader.destroy(publicId)
     } catch (error) {
         console.log("Failed to delete media from cloudinary");
         console.error(error)
     }
 }
 
-
 export const deleteVideoFromCloudinary = async (publicId) => {
     try {
-       await cloudinary.uploader.destroy(publicId, {resource_type: "video"}) 
+        await cloudinary.uploader.destroy(publicId, { resource_type: "video" })
     } catch (error) {
         console.log("Failed to delete media from cloudinary");
         console.error(error)
